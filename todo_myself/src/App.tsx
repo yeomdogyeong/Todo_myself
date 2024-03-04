dd4064c6a0eaebe5d76c9d08bc16da2e724014cb
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-03-04 19:10:04 +0900

    [etc]테스트 커밋

--- todo_myself/src/App.tsx
+++ todo_myself/src/App.tsx
@@ -3,7 +3,7 @@ import { Input } from "./Input";
 function App() {
   return (
     <div className="flex flex-col justify-center items-center">
-      todos
+      Todos
       <Input />
     </div>
   );
